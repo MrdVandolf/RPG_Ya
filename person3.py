3c79eaa0a423b69c3c09d965fe4b0bbedba604af
Dasip <45264175+Dasip@users.noreply.github.com>
2018-12-27 16:01:06 +0300

    Release

--- person3.py
+++ person3.py
@@ -4,11 +4,42 @@
 # Файл с механикой персонажей, здесь задаются их параметры и
 # функции взаимодействия с окном
 # В будущем это будет лишь фрагментом общей программы
-import time
 from random import *
 import sys
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import *
+
+
+class Dialog(QWidget):
+
+    def __init__(self, num, change, state, change2):
+        
+        super().__init__()
+        self.setObjectName("Dialog")
+        self.resize(190, 170)
+        self.pushButton = QPushButton(self)
+        self.pushButton.setGeometry(QtCore.QRect(40, 80, 101, 61))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(lambda: self.close())
+        self.pushButton.pressed.connect(change2)
+        self.pushButton.pressed.connect(change)
+        self.label = QLabel(self)
+        self.label.setGeometry(QtCore.QRect(10, 10, 171, 41))
+        self.label.setObjectName("label")
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+        _translate = QtCore.QCoreApplication.translate
+        self.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.pushButton.setText(_translate("Dialog", "OK"))
+        if state == 'winner':
+            self.label.setText(_translate("Dialog", "Вы получили награду за бой\n"
+                                          "в размере {} золотых").format(num))
+        elif state == 'champion':
+            self.label.setText(_translate("Dialog", "Отныне вы - Чемпион Арены!\n"
+                                          "Ваша награда - {} золотых").format(num))
+        else:
+            self.label.setText(_translate("Dialog", "Вы проиграли бой, обагрив\n"
+                                          "своей кровью пески Арены.").format(num))
 
 
 class Potion:  # зелье
@@ -860,6 +891,13 @@ class UiMainWindow(QWidget):
                 self.player.money += self.rival.money
                 self.attack_button.hide()
                 self.defend_button.hide()
+                if self.rival == self.Rivals[-1]:
+                    self.dialog_window = Dialog(self.rival.money, self.main_open,
+                                                'champion', self.escape)  
+                else:
+                    self.dialog_window = Dialog(self.rival.money, self.main_open,
+                                                'winner', self.escape)
+                self.dialog_window.show()                
             else:
                 if self.action_points < 1:
                     self.action_points = 2
@@ -888,6 +926,8 @@ class UiMainWindow(QWidget):
             self.textBrowsers.append('Вы проиграли бой.')
             self.attack_button.hide()
             self.defend_button.hide()
+            self.dialog_window = Dialog(0, self.main_open,'loser', self.escape) 
+            self.dialog_window.show()  
         else:
             if self.action_points < 1:
                 self.action_points = 2
